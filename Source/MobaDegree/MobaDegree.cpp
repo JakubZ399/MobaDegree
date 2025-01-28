13cd4a86994007adf762c973840d6e160fca84ef
kubaz <zurawikjakub@gmail.com>
2025-01-28 19:21:27 +0100

    Start Project

---
+++ Source/MobaDegree/MobaDegree.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MobaDegree.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobaDegree, "MobaDegree" );
+
+DEFINE_LOG_CATEGORY(LogMobaDegree)
+ 
