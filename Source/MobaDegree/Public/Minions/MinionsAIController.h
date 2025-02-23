5dd9d2be6310895d12d238255f9b7e75ccdff33b
kubaz <zurawikjakub@gmail.com>
2025-02-23 03:26:47 +0100

    Minions Base

---
+++ Source/MobaDegree/Public/Minions/MinionsAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MinionsAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBADEGREE_API AMinionsAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
