a443ba539f74cdac20cf31c6c3abfcee52fa87d1
kubaz <zurawikjakub@gmail.com>
2025-04-28 17:53:51 +0200

    Lobby 2

--- Source/MobaDegree/MobaDegreeGameMode.h
+++ Source/MobaDegree/MobaDegreeGameMode.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "MobaDegreeGameMode.generated.h"
 
 UCLASS(minimalapi)
-class AMobaDegreeGameMode : public AGameModeBase
+class AMobaDegreeGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
