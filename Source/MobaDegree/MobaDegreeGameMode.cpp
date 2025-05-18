e2f98d4af11848fe5bf8e6bcfaab27b73f143bc9
kubaz <zurawikjakub@gmail.com>
2025-05-18 21:01:55 +0200

    PlayerController Fix

--- Source/MobaDegree/MobaDegreeGameMode.cpp
+++ Source/MobaDegree/MobaDegreeGameMode.cpp
@@ -11,22 +11,6 @@
 
 AMobaDegreeGameMode::AMobaDegreeGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = AMobaDegreePlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Player/BP_MobaPlayerCharacterBase"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/Blueprints/Player/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
 }
 
 void AMobaDegreeGameMode::ServerTravel(FString Map)
