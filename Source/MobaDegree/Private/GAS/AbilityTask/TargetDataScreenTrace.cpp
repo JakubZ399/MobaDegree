1509486d0d847c1c5cfd6c072889eca7f9216a52
kubaz <zurawikjakub@gmail.com>
2025-09-22 23:07:24 +0200

    Screen Trace & Khaimera Polishing & Murdock Polishing

--- Source/MobaDegree/Private/GAS/AbilityTask/TargetDataScreenTrace.cpp
+++ Source/MobaDegree/Private/GAS/AbilityTask/TargetDataScreenTrace.cpp
@@ -2,39 +2,80 @@
 
 
 #include "GAS/AbilityTask/TargetDataScreenTrace.h"
+#include "AbilitySystemComponent.h"
 
-UTargetDataScreenTrace* UTargetDataScreenTrace::GetTargetDataFromScreenTrace(UGameplayAbility* OwningAbility, float TraceDistance)
+UTargetDataScreenTrace* UTargetDataScreenTrace::CreateTargetDataScreenTrace(UGameplayAbility* OwningAbility, float InTraceDistance)
 {
-	UTargetDataScreenTrace* MyObj = NewAbilityTask<UTargetDataScreenTrace>(OwningAbility);
-	MyObj->TraceDistance = TraceDistance;
-	return MyObj;
+    UTargetDataScreenTrace* Task = NewAbilityTask<UTargetDataScreenTrace>(OwningAbility);
+    Task->TraceDistance = InTraceDistance;
+    return Task;
 }
 
 void UTargetDataScreenTrace::Activate()
 {
-	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
-	if (bIsLocallyControlled)
-	{
-		SendDataFromScreenTrace();
-	}
-	else
-	{
-		
-	}
-	
-	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+    const bool bLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+    if (bLocallyControlled)
+    {
+        SendScreenTraceData(); // klient przygotowuje i wysyła TargetData
+    }
+    else
+    {
+        const FGameplayAbilitySpecHandle SpecHandle = GetAbilitySpecHandle();
+        const FPredictionKey PredKey = GetActivationPredictionKey();
 
-	int32 SizeX, SizeY;
-	PC->GetViewportSize(SizeX, SizeY);
+        AbilitySystemComponent->AbilityTargetDataSetDelegate(SpecHandle, PredKey)
+            .AddUObject(this, &UTargetDataScreenTrace::OnTargetDataReplicatedCallback);
 
-	FVector WorldLocation, WorldDirection;
-	PC->DeprojectScreenPositionToWorld(SizeX / 2, SizeY / 2, WorldLocation, WorldDirection);
-
-	const FVector EndLocation = WorldLocation + (WorldDirection * TraceDistance);
-
-	ValidData.Broadcast(WorldLocation, EndLocation);
+        const bool bAlreadySet = AbilitySystemComponent->CallReplicatedTargetDataDelegatesIfSet(SpecHandle, PredKey);
+        if (!bAlreadySet)
+        {
+            SetWaitingOnRemotePlayerData();
+        }
+    }
 }
 
-void UTargetDataScreenTrace::SendDataFromScreenTrace()
+void UTargetDataScreenTrace::SendScreenTraceData()
 {
+    if (!AbilitySystemComponent.IsValid()) { EndTask(); return; }
+
+    FScopedPredictionWindow ScopedPred(AbilitySystemComponent.Get());
+
+    APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+    if (!PC) { EndTask(); return; }
+
+    int32 SizeX=0, SizeY=0;
+    PC->GetViewportSize(SizeX, SizeY);
+
+    FVector WorldLoc, WorldDir;
+    PC->DeprojectScreenPositionToWorld(SizeX * 0.5f, SizeY * 0.5f, WorldLoc, WorldDir);
+    WorldDir.Normalize();
+
+    const FVector EndLoc = WorldLoc + (WorldDir * TraceDistance);
+    
+    FGameplayAbilityTargetDataHandle Handle;
+    Handle.Add(new FGameplayAbilityTargetData_ScreenTrace(WorldLoc, EndLoc));
+
+    AbilitySystemComponent->ServerSetReplicatedTargetData(
+        GetAbilitySpecHandle(),
+        GetActivationPredictionKey(),
+        Handle,
+        FGameplayTag(),
+        AbilitySystemComponent->ScopedPredictionKey);
+
+    if (ShouldBroadcastAbilityTaskDelegates())
+    {
+        ValidData.Broadcast(Handle);
+    }
+}
+
+void UTargetDataScreenTrace::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag /*ActivationTag*/)
+{
+    // Serwer (lub drugi koniec) konsumuje dane
+    AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
+
+    if (ShouldBroadcastAbilityTaskDelegates())
+    {
+        ValidData.Broadcast(DataHandle);
+    }
+    EndTask();
 }
--- Source/MobaDegree/Public/GAS/AbilityTask/TargetDataScreenTrace.h
+++ Source/MobaDegree/Public/GAS/AbilityTask/TargetDataScreenTrace.h
@@ -4,31 +4,82 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/Tasks/AbilityTask.h"
+#include "Abilities/GameplayAbilityTargetTypes.h"
 #include "TargetDataScreenTrace.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FScreenTraceDataSignature, const FVector&, StartTracePosition, const FVector&, EndTracePosition);
+USTRUCT(BlueprintType)
+struct FGameplayAbilityTargetData_ScreenTrace : public FGameplayAbilityTargetData
+{
+	GENERATED_BODY()
 
-/**
- * 
- */
+	// Mniej danych po sieci + zgodne z NetSerialize
+	UPROPERTY(BlueprintReadWrite)
+	FVector_NetQuantize Start = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector_NetQuantize End   = FVector::ZeroVector;
+
+	FGameplayAbilityTargetData_ScreenTrace() {}
+	FGameplayAbilityTargetData_ScreenTrace(const FVector& InStart, const FVector& InEnd)
+		: Start(InStart), End(InEnd) {}
+
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return StaticStruct();
+	}
+
+	// === KLUCZOWE: własna serializacja sieciowa ===
+	bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+	{
+		Ar << Start;
+		Ar << End;
+		bOutSuccess = true;
+		return true;
+	}
+
+#if WITH_EDITOR
+	virtual FString ToString() const override
+	{
+		return FString::Printf(TEXT("ScreenTrace: Start=%s End=%s"),
+			*Start.ToString(), *End.ToString());
+	}
+#endif
+};
+
+// === Specjalizacja traits, żeby UE wiedziało, że mamy NetSerialize ===
+template<>
+struct TStructOpsTypeTraits<FGameplayAbilityTargetData_ScreenTrace>
+	: public TStructOpsTypeTraitsBase2<FGameplayAbilityTargetData_ScreenTrace>
+{
+	enum
+	{
+		WithNetSerializer = true,   // <- wymagane
+		WithCopy         = true,    // opcjonalnie, ale praktyczne
+	};
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FScreenTraceDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
+
 UCLASS()
 class MOBADEGREE_API UTargetDataScreenTrace : public UAbilityTask
 {
 	GENERATED_BODY()
 
 public:
-	
-	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta = (DisplayName = "GetTargetDataFromPlayerScreenTrace", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "true"))
-	static UTargetDataScreenTrace* GetTargetDataFromScreenTrace(UGameplayAbility* OwningAbility, float TraceDistance = 10000.0f);
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks",
+			  meta=(DisplayName="TargetDataScreenTrace",
+					HidePin="OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly="true"))
+	static UTargetDataScreenTrace* CreateTargetDataScreenTrace(UGameplayAbility* OwningAbility, float TraceDistance = 10000.f);
 
 	UPROPERTY(BlueprintAssignable)
 	FScreenTraceDataSignature ValidData;
 
 private:
-
 	virtual void Activate() override;
-	void SendDataFromScreenTrace();
+
+	void SendScreenTraceData(); // client → build Handle(Start/End) → replicate
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 
 	UPROPERTY()
-	float TraceDistance;
+	float TraceDistance = 3000.f;
 };
